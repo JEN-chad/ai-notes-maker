6a9907b51319063c04e2522e05d91de4bddb9bb8
JEN-chad <jenishj436@gmail.com>
2025-10-05 15:15:07 +0530

    solved api key issue

--- configs/AiModel.js
+++ configs/AiModel.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
-// const apikey = 'AIzaSyAa5hCmcm2VHCLaCniq1hv2UlolN_lZ0Y0'
+
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
